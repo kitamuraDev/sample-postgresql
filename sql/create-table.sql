e2fe5345ecf3bde0378b4bd5b1876508bd81ddc8
kitamuraDev <masato1999824@icloud.com>
2023-06-03 23:16:39 +0900

    :sparkles: テーブルの構造を変更

--- sql/create-table.sql
+++ sql/create-table.sql
@@ -1,5 +1,8 @@
 CREATE TABLE characters(
     id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
-    create_time DATE
+    age int,
+    voice_actor VARCHAR(50),
+    created_at DATE,
+    updated_at DATE
 );
