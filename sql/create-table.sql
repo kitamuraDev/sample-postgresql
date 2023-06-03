8a4f9c8e2193316b9c6c9f2522720864a6370ecc
kitamuraDev <masato1999824@icloud.com>
2023-06-04 00:06:48 +0900

    :sparkles: データ構造の変更とデフォルトデータセットの変更

--- sql/create-table.sql
+++ sql/create-table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE characters(
     id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255),
-    age int,
+    birthdate VARCHAR(20),
     voice_actor VARCHAR(50),
     created_at DATE,
-    updated_at DATE
+    updated_at DATE DEFAULT CURRENT_DATE
 );
--- sql/insert-data.sql
+++ sql/insert-data.sql
@@ -1,7 +1,22 @@
 -- 挿入
 
-INSERT INTO characters (name, create_time) VALUES ('Mai Sakurajima', '2023/06/02');
-INSERT INTO characters (name, create_time) VALUES ('Sakuta Azusagawa', '2023/06/02');
-INSERT INTO characters (name, create_time) VALUES ('Kaede Azusagawa', '2023/06/02');
-INSERT INTO characters (name, create_time) VALUES ('Nodoka Toyohama', '2023/06/02');
-INSERT INTO characters (name, create_time) VALUES ('Shoko Makinohara', '2023/06/02');
+INSERT INTO characters (name, birthdate, voice_actor, created_at, updated_at)
+  VALUES ('桜島 麻衣', '12月2日', '瀬戸 麻沙美', CURRENT_DATE, CURRENT_DATE);
+
+INSERT INTO characters (name, birthdate, voice_actor, created_at, updated_at)
+  VALUES ('梓川 咲太', '4月10日', '石川 界人', CURRENT_DATE, CURRENT_DATE);
+
+INSERT INTO characters (name, birthdate, voice_actor, created_at, updated_at)
+  VALUES ('梓川 花楓', '11月25日', '久保 ユリカ', CURRENT_DATE, CURRENT_DATE);
+
+INSERT INTO characters (name, birthdate, voice_actor, created_at, updated_at)
+  VALUES ('豊浜 のどか', '3月14日', '内田 真礼', CURRENT_DATE, CURRENT_DATE);
+
+INSERT INTO characters (name, birthdate, voice_actor, created_at, updated_at)
+  VALUES ('牧之原 翔子', '4月10日', '水瀬 いのり', CURRENT_DATE, CURRENT_DATE);
+
+INSERT INTO characters (name, birthdate, voice_actor, created_at, updated_at)
+  VALUES ('古賀 朋絵', '5月23日', '東山 奈央', CURRENT_DATE, CURRENT_DATE);
+
+INSERT INTO characters (name, birthdate, voice_actor, created_at, updated_at)
+  VALUES ('双葉 理央', '10月23日', '種崎 敦美', CURRENT_DATE, CURRENT_DATE);
